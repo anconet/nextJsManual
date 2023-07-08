32d83f166b15bd5d13b1a3ef87ac076efe4595f2
Anthony Anconetani <anthony.anconetani@gmail.com>
2023-07-08 17:11:50 -0500

    Adding some more tailwind stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+
+function Card() {
+
+  return <div><h1 className="bg-slate-500 text-slate-300 rounded-lg p-3 flex items-center">
+    <p className="text-blue-400">Hello</p>, Next.js!</h1>
+  </div>
+}
+
 export default function Page() {
-    return <h1
-        className="
-    bg-slate-500
-    text-slate-300
-    rounded-lg
-    p-3
-    ">
-        Hello, Next.js!</h1>
+  return <div className="bg-slate-600 p-4 grid grid-cols-2 gap-4 rounded-xl">
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+  </div>
 }
